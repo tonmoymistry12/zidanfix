de9ed511775634d0a47856fb06f87c0cb667084c
tonmoy.mistry <tonmoy.mistry12@gmail.com>
2024-10-18 17:35:18 +0530

    testing vercel

--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "835c61dac98095b1"; }
+/******/ 		__webpack_require__.h = function() { return "ee0f72162dd61e2d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import FirstPage from "../component/Landing-Page/First-Page";
 import SecondPage from "../component/Landing-Page/Second-Page";
@@ -12,7 +11,15 @@ export default function Home() {
       <FirstPage />
       <SecondPage />
       {/* <ThirdPage /> */}
-
+      <a
+        href="https://wa.me/9233141733" // Replace with your WhatsApp number
+        target="_blank"
+        rel="noopener noreferrer"
+        className="whatsapp-icon"
+      >
+        
+        <img src={'/images/clipart/whatsapp.svg'} alt="WhatsApp" />
+      </a>
     </BaseLayout>
   );
 }
